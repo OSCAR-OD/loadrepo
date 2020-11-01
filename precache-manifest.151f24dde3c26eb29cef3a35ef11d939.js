20f7db7f86e27ead94bde1a2eab5b59d51534372
OSCAR-OD <oscardeb99@gmail.com>
2020-11-01 22:58:55 +0600

    Updates

---
+++ precache-manifest.151f24dde3c26eb29cef3a35ef11d939.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58cd9b897243c0c88c69ef29dc2f1837",
+    "url": "/loadrepo/index.html"
+  },
+  {
+    "revision": "57d47d04ac927cf5cae2",
+    "url": "/loadrepo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "88181064b79f252ff65d",
+    "url": "/loadrepo/static/js/2.a6c3c362.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/loadrepo/static/js/2.a6c3c362.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57d47d04ac927cf5cae2",
+    "url": "/loadrepo/static/js/main.bbf167b2.chunk.js"
+  },
+  {
+    "revision": "e3eb63e61789a1761076",
+    "url": "/loadrepo/static/js/runtime-main.2dd1f8c0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/loadrepo/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/OSCAR-OD/loadrepo.git/precache-manifest.6ab87b3250d12349d2e3f4daf531ef9e.js"
+  "/loadrepo/precache-manifest.151f24dde3c26eb29cef3a35ef11d939.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/OSCAR-OD/loadrepo.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/loadrepo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
